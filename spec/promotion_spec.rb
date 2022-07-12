4d5d1f3a53b8f6d25e33143c6123464eb1f4ff64
RitaHartnell <mel.soriano6123@gmail.com>
2022-07-12 16:50:32 -0400

    third test

--- spec/promotion_spec.rb
+++ spec/promotion_spec.rb
@@ -10,16 +10,15 @@ RSpec.describe Promotion, "#qualifying_amount" do
 end 
 
 RSpec.describe Promotion, "#add_product" do
-    context "not combo promotion" do
+    it "successfully adds a product" do
+        product = "cheese"
+        promo = Promotion.new(false, true, 2, 15)
+        promo.add_product(product)
+        expect(promo.products[0]).to eq("cheese")
+    end
+    context " when not combo promotion" do
         #TODO - implement product
-        #TODO - implement check for product existing in non combo promo
-        it "successfully adds a product" do
-            product = "cheese"
-            promo = Promotion.new(false, true, 2, 15)
-            promo.add_product(product)
-            expect(promo.products[0]).to eq("cheese")
-        end
-
+        #TODO - implement check for product existing in non combo promo - DONE
         it "checks if there is already a product and returns an error message if so" do
             product1 = "cheese"
             product2 = "milk"
@@ -27,6 +26,15 @@ RSpec.describe Promotion, "#add_product" do
             promo.add_product(product1)
             expect{promo.add_product(product2)} .to output("cannot add 2nd product to non combo promotion").to_stdout
         end
-
+    end
+    context " when is combo promo" do
+        it "can add second product to the list" do
+            product1 = "cheese"
+            product2 = "milk"
+            promo = Promotion.new(true, false, 30)
+            promo.add_product(product1)
+            promo.add_product(product2)
+            expect(promo.products[1]).to eq("milk")
+        end
     end
 end
