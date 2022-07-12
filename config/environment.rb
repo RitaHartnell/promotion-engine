bd1208d4cfc547d5588f7aee016e33b9f5ab8137
RitaHartnell <mel.soriano6123@gmail.com>
2022-07-12 12:01:16 -0400

    added rails / rails rspec (plain rspec can't use active record anymore)

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
-require 'bundler'
-Bundler.require
+# Load the Rails application.
+require_relative 'application'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
-
-ActiveRecord::Base.logger = nil
+# Initialize the Rails application.
+Rails.application.initialize!
