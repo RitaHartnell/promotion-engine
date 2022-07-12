ffc279c4952b81b0823e5bb7bda3265975924d0d
RitaHartnell <mel.soriano6123@gmail.com>
2022-07-12 12:31:05 -0400

    overcomplicating things again

--- db/migrate/20220712143158_create_join_table_products_promotions.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableProductsPromotions < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :products, :promotions do |t|
-    # t.index [:student_id, :course_id]
-    # t.index [:course_id, :student_id]
-    end
-  end
-end
